15d141d23d4de936cac7cad6d5ff585c3637d1b6
David Anthoff <anthoff@berkeley.edu>
2023-01-03 19:42:47 -0800

    Progress

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-CHANNEL=${CHANNEL:-"release"}
-
-curl -fsSL https://install.julialang.org | sh -s -- --yes --default-channel "${VERSION}"
---
+++ src/julia/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+
+CHANNEL=${CHANNEL:-"release"}
+
+apt_get_update()
+{
+    echo "Running apt-get update..."
+    apt-get update -y
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            apt-get update -y
+        fi
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt_get_update
+
+check_packages curl ca-certificates
+
+curl -fsSL https://install.julialang.org | sh -s -- --yes --default-channel "${VERSION}"
