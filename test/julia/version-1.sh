91ed498c1400383db11746170f89f571ba828469
eitsupi <ts1s1andn@gmail.com>
2023-01-06 22:01:58 +0900

    update tests

--- test/julia/version-1.sh
+++ test/julia/version-1.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Feature-specific tests
-check "version" julia --version
+check "version" julia --version | grep "1."
 
 # Report result
 reportResults
