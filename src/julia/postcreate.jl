52de3bb240667a5d98e11c471d0e5c67ca95b57d
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-06-26 01:43:54 -0300

    Take postcreate.jl from julia-vscode/julia-devcontainer

---
+++ src/julia/postcreate.jl
@@ -0,0 +1,26 @@
+#!/usr/local/julia/bin/julia
+
+using Pkg
+
+function has_manifest_file()
+    return isfile(joinpath(pwd(),"Manifest.toml")) || isfile(joinpath(pwd(),"JuliaManifest.toml"))
+end
+
+function has_project_file()
+    return isfile(joinpath(pwd(),"Project.toml")) || isfile(joinpath(pwd(),"JuliaProject.toml"))
+end
+
+function is_package()
+    return has_project_file() && !has_manifest_file()
+end
+
+function is_app()
+    return has_project_file() && has_manifest_file()
+end
+
+if is_app()
+    Pkg.activate(pwd())
+    Pkg.instantiate()    
+elseif is_package()
+    Pkg.develop(path=pwd())    
+end
